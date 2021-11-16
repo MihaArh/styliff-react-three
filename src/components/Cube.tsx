5050c25a558ef4e535885710f23c890c87124f12
MihaArh <miha.arh06@gmail.com>
2021-11-16 15:53:43 +0100

    Add: Cube component

---
+++ src/components/Cube.tsx
@@ -0,0 +1,40 @@
+import Plane from './Plane';
+
+const Cube = () => {
+  return (
+    <>
+      <Plane
+        color="blue"
+        position={[50, 0, 50]}
+        rotation={[-Math.PI / 2, 0, 0]}
+      />
+      <Plane
+        color="red"
+        position={[50, 100, 50]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+      <Plane
+        color="yellow"
+        position={[100, 50, 50]}
+        rotation={[0, -Math.PI / 2, 0]}
+      />
+      <Plane
+        color="pink"
+        position={[0, 50, 50]}
+        rotation={[0, Math.PI / 2, 0]}
+      />
+      <Plane
+        color="green"
+        position={[50, 50, 0]}
+        rotation={[0, 0, Math.PI / 2]}
+      />
+      <Plane
+        color="lightblue"
+        position={[50, 50, 100]}
+        rotation={[-Math.PI, 0, 0]}
+      />
+    </>
+  );
+};
+
+export default Cube;
