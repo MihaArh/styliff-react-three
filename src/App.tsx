2d14c849685292e053e263867deb1c42044a66c4
MihaArh <miha.arh06@gmail.com>
2021-11-16 15:53:15 +0100

    Add: basic components and controles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.scss';
 import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stats } from '@react-three/drei';
 import Plane from './components/Plane';
 
 const App = function () {
   return (
-    <Canvas>
+    <Canvas
+      mode="concurrent"
+      shadows
+      gl={{ alpha: false }}
+      camera={{ position: [200, 200, 200] }}
+    >
+      <OrbitControls
+        addEventListener={undefined}
+        hasEventListener={undefined}
+        removeEventListener={undefined}
+        dispatchEvent={undefined}
+      />
+      <hemisphereLight intensity={0.35} />
+      <spotLight
+        position={[200, 1000, 200]}
+        angle={0.3}
+        penumbra={1}
+        intensity={2}
+        castShadow
+        shadow-mapSize-width={256}
+        shadow-mapSize-height={256}
+      />
+      <pointLight position={[-30, 0, -30]} intensity={0.5} />
+      <axesHelper scale={500} />
+      <Stats />
       <Plane
         color="blue"
         position={[50, 0, 50]}
